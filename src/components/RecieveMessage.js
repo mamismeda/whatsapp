b6413152a460e9905381636eda3233f64d54daa1
Aleksandre <amamismeda@gmail.com>
2023-05-15 18:19:37 +0400

    end of whatsapp with greenapi

--- src/components/RecieveMessage.js
+++ src/components/RecieveMessage.js
@@ -5,28 +5,30 @@ const RecieveMessage = () => {
   const id = localStorage.getItem("userId");
   const api = localStorage.getItem("userApi");
 
-  const fetchData = async () => {
-    const url = `https://api.green-api.com/waInstance${id}/ReceiveNotification/${api}`;
-
-    try {
-      const response = await axios.get(url);
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   useEffect(() => {
+    const fetchData = async () => {
+      const url = `https://api.green-api.com/waInstance${id}/ReceiveNotification/${api}`;
+
+      try {
+        const response = await axios.get(url);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
     fetchData();
+
   }, []);
 
   return (
-    <div>
+    <div className="mt-5 text-center">
       <ul>
-        <li>waiting for answer...</li>
+        <li>waiting for answer... </li>
       </ul>
     </div>
   );
+
+ 
 };
 
 export default RecieveMessage;
